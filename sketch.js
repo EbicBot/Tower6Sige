a16a1857786bb1085c254cee33dfbdeb8f6dd4b2
Varun <vjayaram2006@gmail.com>
2020-07-20 17:29:52 -0400

    commit on master

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+var box1,box2,box3,box4,box5,box6,box7,box8,box9,box10
+var box11,box12,box13,box14,box15,box16,box17,box18,box19,box20
+var box21,box22,box23,box24,box25
+
 function setup() {
   createCanvas(800,400);
   createSprite(400, 200, 50, 50);
